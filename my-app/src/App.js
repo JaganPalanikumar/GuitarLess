f2ddd4beb7e80e4deb4fc4152854a657906b6382
JaganPalanikumar <jaganpalkumar@gmail.com>
2025-10-23 01:06:33 -0500

    updated frontend js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -60,7 +60,7 @@ function App() {
       setElapsedSeconds(prev => prev + 1);
     }, 1000);
 
-    const eventSource = new EventSource('http://guitarlessappdemo.westus.azurecontainer.io:8000/progress');
+    const eventSource = new EventSource('https://guitarlessappdemo.westus.azurecontainer.io/progress');
     eventSourceRef.current = eventSource;
 
     eventSource.onmessage = (event) => {
@@ -77,7 +77,7 @@ function App() {
     };
 
     try {
-      const response = await fetch('http://guitarlessappdemo.westus.azurecontainer.io:8000/songprocessing', {
+      const response = await fetch('https://guitarlessappdemo.westus.azurecontainer.io/songprocessing', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({ url })
